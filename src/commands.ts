c8c631c3969a0264d62cfa0d74e583c697401d50
bftsgt <bfutasz@gmail.com>
2024-10-21 16:42:20 +0200

    task3 done: details command implemented

--- src/commands.ts
+++ src/commands.ts
@@ -14,3 +14,23 @@ export async function list(store: Store<RecipeType[]>, args: string[]) {
   console.log('Your recipes:');
   console.log(formatted);
 }
+
+export async function details(store: Store<RecipeType[]>, args: string[]) {
+  if (args.length !== 1) {
+    throw new AppError("Error: The details command should have one argument (the id number).");
+  }
+  if (isNaN(parseInt(args[0]))){
+    throw new AppError(
+      "Error: The id should be a number)."
+    );
+  }
+  const recipe = new Recipe(store);
+  const recipes = await recipe.readAll();
+  let id: number = Number(args[0]) 
+  const matchingRecipe = recipes.find(r => r.id === id)
+  if (!matchingRecipe) {
+    throw new AppError(`Error: recipe not found with id: ${id} `);
+  }
+  console.log(`ID: ${matchingRecipe.id} \n Name: ${matchingRecipe.name}`)
+
+};
