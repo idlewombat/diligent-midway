1bf08a5c2558a4a5701cb694a5d0990780a82b92
bftsgt <bfutasz@gmail.com>
2024-10-21 16:28:34 +0200

    task2 done, extra arguments in list command throws error

--- src/commands.ts
+++ src/commands.ts
@@ -1,7 +1,11 @@
+import { AppError } from "./app.error";
 import { Recipe, RecipeType } from "./recipe";
 import { Store } from "./stores/store.type";
 
 export async function list(store: Store<RecipeType[]>, args: string[]) {
+  if (args.length > 0) {
+    throw new AppError("Error: The list command should not have any argument.");
+  }
   const recipe = new Recipe(store);
   const recipes = await recipe.readAll();
   const formatted = recipes
