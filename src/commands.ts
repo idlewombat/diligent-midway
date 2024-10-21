60b5987818c58848e4616f7532fed4b3dbd4ec97
bftsgt <bfutasz@gmail.com>
2024-10-21 16:56:30 +0200

    task4 done

--- src/commands.ts
+++ src/commands.ts
@@ -2,6 +2,8 @@ import { AppError } from "./app.error";
 import { Recipe, RecipeType } from "./recipe";
 import { Store } from "./stores/store.type";
 
+export type CreateRecipeType = Omit<RecipeType, "id">;
+
 export async function list(store: Store<RecipeType[]>, args: string[]) {
   if (args.length > 0) {
     throw new AppError("Error: The list command should not have any argument.");
@@ -32,5 +34,31 @@ export async function details(store: Store<RecipeType[]>, args: string[]) {
     throw new AppError(`Error: recipe not found with id: ${id} `);
   }
   console.log(`ID: ${matchingRecipe.id} \n Name: ${matchingRecipe.name}`)
-
 };
+
+export async function create(store: Store<RecipeType[]>, args: string[]) {
+  if (args.length !== 1) {
+    throw new AppError(
+      "Error: The create command only needs the name of the recipe."
+    );
+  }
+
+  const recipeName = args[0];
+  const recipe = new Recipe(store);
+  const recipes = await recipe.readAll();
+  let newId = 1;
+
+  if (recipes.length > 0) {
+    const maxId = Math.max(...recipes.map((r) => r.id));
+    newId = maxId + 1;
+  }
+  
+  const newRecipe: RecipeType = {
+    id: newId,
+    name: recipeName,
+  };
+  console.log(newRecipe)
+
+  console.log(`ID: ${newRecipe.id}`);
+  console.log(`Name: ${newRecipe.name}`);
+}
